ea66a13ada99b6cfd26285cd9e347fea69d52150
Abhishek Kadam <abhimkdm@gmail.com>
2020-02-11 19:46:13 +0530

    If conditions & Common Functions

--- Js/login.js
+++ Js/login.js
@@ -1,6 +1,29 @@
 function LogIn() {
-  var email = document.getElementById("email").value;
-  var pass = document.getElementById("pass").value;
-  document.getElementById("result").innerHTML =
-    "<p>Email :" + email + "&nbsp;&nbsp;&nbsp;" + "Password:" + pass + "</p>";
+  var emailVal = getVal("email");
+  var passVal = getVal("pass");
+  var msg = "";
+
+  //Vaidate user
+  document.getElementById("result").innerHTML = validateUser(emailVal, passVal);
+
+  // document.getElementById("result").innerHTML =
+  //   "<p>Email :" + email + "&nbsp;&nbsp;&nbsp;" + "Password:" + pass + "</p>";
+}
+
+/**Common functions**/
+
+//To the vlues of the textbox.
+function getVal(id) {
+  return document.getElementById(id).value;
+}
+
+// To Validate user
+function validateUser(email, pass) {
+  if (email == "abc@gmail.com" && pass == "password") {
+    msg = "<p>Login Succesfull</p>";
+  } else {
+    msg = "<p>Login UnSuccesful</p>";
+  }
+
+  return msg;
 }
