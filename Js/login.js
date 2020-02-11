1c1619cbd7eb82aebab3e9eac2ae498b7d22e31d
Abhishek Kadam <abhimkdm@gmail.com>
2020-02-11 20:12:35 +0530

    Switch & IF Else

--- Js/login.js
+++ Js/login.js
@@ -1,7 +1,6 @@
 function LogIn() {
   var emailVal = getVal("email");
   var passVal = getVal("pass");
-  var msg = "";
 
   //Vaidate user
   document.getElementById("result").innerHTML = validateUser(emailVal, passVal);
@@ -17,13 +16,34 @@ function getVal(id) {
   return document.getElementById(id).value;
 }
 
+//If Else Example
 // To Validate user
 function validateUser(email, pass) {
-  if (email == "abc@gmail.com" && pass == "password") {
+  if (email == "admin" && pass == "admin") {
     msg = "<p>Login Succesfull</p>";
+    Legend("admin");
+  } else if (email == "raghu" && pass == "admin") {
+    msg = "<p>Login Succesfull</p>";
+    Legend("raghu");
   } else {
     msg = "<p>Login UnSuccesful</p>";
+    Legend("");
   }
 
   return msg;
+}
+
+//Switch Example
+function Legend(user) {
+  switch (user) {
+    case "admin":
+      document.getElementById("loginUser").innerHTML = "Welcome Admin";
+      break;
+    case "raghu":
+      document.getElementById("loginUser").innerHTML = "Welcome Raghu";
+      break;
+    default:
+      document.getElementById("loginUser").innerHTML = "Welcome Guest";
+      break;
+  }
 }
